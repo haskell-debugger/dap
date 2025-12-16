86542f291320497609b6b84f7fe6d67983a5d2f9
dmjio <code@dmj.io>
2025-12-15 23:39:33 -0600

    Use `haskellPackages`

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
 let
-  dap = pkgs.haskell.packages.ghc966.callCabal2nix "dap" ./. {};
+  dap = pkgs.haskellPackages.callCabal2nix "dap" ./. {};
 in
 {
   inherit dap pkgs;
